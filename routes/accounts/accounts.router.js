d2973b52e478f497399df003759907e56946b44f
Yusufmukhi <yusufmukhi1@gmail.com>
2026-01-01 11:21:03 +0530

    Account success Mobile Responsive

--- routes/accounts/accounts.router.js
+++ routes/accounts/accounts.router.js
@@ -14,8 +14,8 @@ const router = express.Router();
 router.get("/accounts/:id", account);
 
 // create account
-router.get("/create-account", accountdetalis);
-router.post("/create-account", createaccount);
+router.get("/create-account/:id", accountdetalis);
+router.post("/create-account/:id", createaccount);
 
 // account details (account_id is OK)
 router.get("/details/:accid", deatlis);
